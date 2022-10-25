f3c3b9472918135d7069da8c1ffadddddbbddc2d
Rodrigo <rodrigo.sangali@gmail.com>
2022-10-25 08:36:31 -0300

    adding POJOS, Closes #2.

---
+++ src/main/java/sangali/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package sangali.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/sangali/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package sangali.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
