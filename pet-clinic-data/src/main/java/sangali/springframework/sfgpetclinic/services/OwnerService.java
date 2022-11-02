049a0f18832a80eb4c5edd2db32ba960e9c6947b
Rodrigo <rodrigo.sangali@gmail.com>
2022-11-02 08:28:51 -0300

    Refactor Services to Commom Interface

--- pet-clinic-data/src/main/java/sangali/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/sangali/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,20 +2,7 @@ package sangali.springframework.sfgpetclinic.services;
 
 import sangali.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
-
-
-
 
 }
--- pet-clinic-data/src/main/java/sangali/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/sangali/springframework/sfgpetclinic/services/VetService.java
@@ -2,16 +2,6 @@ package sangali.springframework.sfgpetclinic.services;
 
 import sangali.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-
+public interface VetService extends CrudService<Vet, Long>{
 
 }
