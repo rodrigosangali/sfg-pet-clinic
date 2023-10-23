8e180b47de6eb3d9c6ec1fcd0eebc8a6ed3a4f46
Rodrigo <rodrigo.sangali@gmail.com>
2023-10-23 09:44:39 -0300

    126 Spring Pet Clinic - Fixing Broken Links

--- pet-clinic-web/src/main/java/sangali/springframework/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sangali/springframework/controllers/OwnerController.java
@@ -21,6 +21,7 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
