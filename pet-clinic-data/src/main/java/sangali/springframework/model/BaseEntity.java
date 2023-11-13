83ba1c4e9b87f167a79a5ff89dc81f70f716dc6c
Rodrigo <rodrigo.sangali@gmail.com>
2023-11-13 09:21:32 -0300

    145 Spring Pet Clinic - Created Base Entity

--- pet-clinic-data/src/main/java/sangali/springframework/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/sangali/springframework/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package sangali.springframework.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
     public Long getId() {
